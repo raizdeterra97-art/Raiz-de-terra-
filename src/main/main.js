d49d9ea3ee904db13249e09e3994cd3a982cd1c5
raizdeterra97-art <raizdeterra97@gmail.com>
2026-03-23 06:21:24 +0000

    Create Electron main process file main.js

---
+++ src/main/main.js
@@ -0,0 +1,27 @@
+// Electron main process
+
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    });
+
+    // Load your index.html file here
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
